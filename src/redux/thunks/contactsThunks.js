0ee243eac0a6d6088c4014e5e610e0dda5bbe028
xanderkiev2022 <301019a@gmail.com>
2023-02-06 13:21:42 +0200

    thunk

--- src/redux/thunks/contactsThunks.js
+++ src/redux/thunks/contactsThunks.js
@@ -1,5 +1,39 @@
-// https://63e0dbed59bb472a742a7055.mockapi.io/contacts/:endpoint
-
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 export const getContactsThunk = createAsyncThunk('contacts/getContacts', () => getContacts);
+
+axios.defaults.baseURL = 'https://63e0dbed59bb472a742a7055.mockapi.io';
+
+export const fetchContacts = createAsyncThunk('contacts/fetchAll', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get('/contacts');
+    return response.data;
+  } catch (e) {
+    return thunkAPI.rejectWithValue(e.message);
+  }
+});
+
+export const addContact = createAsyncThunk('contacts/addContact', async (contact, thunkAPI) => {
+  try {
+    const response = await axios.post('/contacts', { contact });
+    return response.data;
+  } catch (e) {
+    return thunkAPI.rejectWithValue(e.message);
+  }
+});
+
+export const deleteContact = createAsyncThunk('contacts/deleteContact', async (contactId, thunkAPI) => {
+  try {
+    const response = await axios.delete(`/contacts/${contactId}`);
+    return response.data;
+  } catch (e) {
+    return thunkAPI.rejectWithValue(e.message);
+  }
+});
+
+// Використовуй функцію createAsyncThunk для оголошення асинхронних генераторів екшенів та виконання HTTP-запитів. Обробку екшенів та зміну даних у стані Redux зроби за допомогою createSlice.
+
+// fetchContacts - одержання масиву контактів (метод GET) запитом. Базовий тип екшену "contacts/fetchAll".
+// addContact - додавання контакту (метод POST). Базовий тип екшену "contacts/addContact".
+// deleteContact - видалення контакту (метод DELETE). Базовий тип екшену "contacts/deleteContact".
